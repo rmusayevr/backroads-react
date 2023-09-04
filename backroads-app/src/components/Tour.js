b9db70b5aedd383128da8174de501cf14ea864cf
rmusayevr <rmusayevr19@gmail.com>
2023-09-04 19:11:54 +0400

    first react app finished

---
+++ backroads-app/src/components/Tour.js
@@ -0,0 +1,25 @@
+const Tour = ({ image, date, title, about, location, duration, price }) => {
+    return (
+        <article className="tour-card">
+            <div className="tour-img-container">
+                <img src={image} className="tour-img" alt="" />
+                <p className="tour-date">{date}</p>
+            </div>
+            <div className="tour-info">
+                <div className="tour-title">
+                    <h4>{title}</h4>
+                </div>
+                <p>{about}</p>
+                <div className="tour-footer">
+                    <p>
+                        <span><i className="fas fa-map"></i></span> {location}
+                    </p>
+                    <p>{duration} days</p>
+                    <p>from ${price}</p>
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default Tour
